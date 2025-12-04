2d143a2f195750aa6bcc6f922031415378dfe6a6
AdelineMeisterzheim403 <meistertzheimadeline@gmail.com>
2025-12-04 21:19:05 +0100

    pull develop

--- ALTF4/src/App.jsx
+++ ALTF4/src/App.jsx
@@ -15,7 +15,8 @@ function App() {
         {/* exemple d'une autre route */}
         {/* <Route path="/about" element={<About />} /> */}
 
-        <Route path="*" element={<Home />} /> // Route 404 redirige page principale
+        <Route path="*" element={<Home />} /> {/* Route 404 redirige page principale */}
+        <Route path="/groovebox" element={<GrooveBoxMain/>}/>
       </Routes>
     </div>
   );
