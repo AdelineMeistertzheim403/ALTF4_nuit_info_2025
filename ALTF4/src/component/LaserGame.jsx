efad2eae19d1bbf27db29c0b5a219a2da071f943
jules.corneille <jules.corneille@uha.fr>
2025-12-04 21:22:23 +0100

    composant LaserGame

---
+++ ALTF4/src/component/LaserGame.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+
+function LaserGame() {
+  return (
+    <div>
+      <h1>Code du Lasergame</h1>
+      
+    </div>
+  );
+}
+
+export default LaserGame;
