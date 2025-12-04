a58895bb82ed8de20f94600bfc5491cef3b3dc44
jules.corneille <jules.corneille@uha.fr>
2025-12-05 00:28:32 +0100

    ajout de la route groovebox

--- ALTF4/src/App.jsx
+++ ALTF4/src/App.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
+import GrooveBoxMain from './pages/GrooveBoxMain'
+
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/groovebox" element={<GrooveBoxMain/>}/>
 
         {/* exemple d'une autre route */}
         {/* <Route path="/about" element={<About />} /> */}
