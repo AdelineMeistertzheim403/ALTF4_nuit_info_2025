38b73356fc6ecca319926cc3f16e905c42ba97e1
Emma Buscot <emmabuscot@gmail.com>
2025-12-05 04:39:07 +0100

    Ajout route dans App.jsx

--- ALTF4/src/App.jsx
+++ ALTF4/src/App.jsx
@@ -6,6 +6,8 @@ import GrooveBoxMain from './pages/GrooveBoxMain'
 
 import Scenario from './pages/Scenarios/Scenario';
 
+import Defis3 from './pages/Defis3';
+
 function App() {
   return (
     <div className="App">
@@ -13,6 +15,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/scenario" element={<Scenario />} />
         <Route path="/groovebox" element={<GrooveBoxMain/>}/>
+
+        <Route path="/defis3" element={<Defis3/>}/>
 
         {/* exemple d'une autre route */}
         {/* <Route path="/about" element={<About />} /> */}
